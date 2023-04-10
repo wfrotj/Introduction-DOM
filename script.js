0532746e967037d3a5ffc18d72fca9418566a6ae
wfortj <trebrodrigo@gmail.com>
2023-04-10 21:41:39 +0800

    appending

--- script.js
+++ script.js
@@ -40,41 +40,18 @@ body.appendChild(container);
 */
 
 const body = document.querySelector("body");
+const header = document.createElement("header");
 
-const container = document.createElement("div");
-container.setAttribute("id", "container");
+body.appendChild(header);
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
+const logo = document.createElement("h1");
+logo.textContent = "Logo";
+header.appendChild(logo);
 
-container.appendChild(content);
+const menu = document.createElement("menu");
 
-const p = document.createElement("p");
-p.textContent = "Hey, I'm red!";
-p.style.color = "red";
+header.appendChild(menu);
 
-container.appendChild(p);
+const list = document.createElement("listing");
 
-const h3 = document.createElement("h3");
-h3.textContent = "I'm a blue h3!";
-h3.style.color = "blue";
-
-container.appendChild(h3);
-
-const div = document.createElement("div");
-div.style.cssText = "border: 1px solid black; background-color: pink;";
-
-container.appendChild(div);
-
-const h1 = document.createElement("h1");
-h1.textContent = "I'm in a div";
-
-div.appendChild(h1);
-
-const p2 = document.createElement("p");
-p2.textContent = "ME TOO!";
-
-div.appendChild(p2);
-
-body.appendChild(container);
+menu.appendChild(listing);
